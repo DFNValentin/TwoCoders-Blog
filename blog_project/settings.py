2faee1283e387908fbba0fa3b959979b323bd244
Valentin <dumitracheedi2@gmail.com>
2023-08-19 23:26:13 +0300

    added contact functionality

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -43,6 +43,14 @@ SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_CONTENT_POLICY = "frame-ancestors 'self'"
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'dumitrache.edi.valentin@gmail.com'
+EMAIL_HOST_PASSWORD = 'xspluvgagzzumstn'
+
+
 CORS_ALLOWED_ORIGINS = [
     'https://twc-blog.up.railway.app',
     'https://two-coders.site',
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
 
 from blog.models import Post, Resources
+
+from django.core.mail import send_mail
+
+from blog.forms import ContactForm
+
+from django.template.loader import render_to_string
+
 
 # Create your views here.
 
@@ -15,7 +22,28 @@ def index(request):
 
 
 def contact(request):
-    return render(request, "contact.html")
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            subject = f'New message on Two Coders Blogs '
+
+            html = render_to_string('contactForm.html', {
+                'name': name,
+                'email': email,
+                'message': message
+            })
+
+            send_mail(subject, message, 'valentin@two-coders.site',
+                      ['valentin@two-coders.site'], html_message=html)
+
+            return redirect('index')
+    else:
+        form = ContactForm()
+    return render(request, 'contact.html', {'form': form})
 
 
 def resources(request):
