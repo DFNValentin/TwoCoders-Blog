b5d375ba907319b0f95bb1503ec861891d253c8d
Valentin <dumitracheedi2@gmail.com>
2023-08-19 11:49:09 +0300

    verification from mailjet

--- main_app/views.py
+++ main_app/views.py
@@ -14,8 +14,8 @@ def index(request):
     })
 
 
-def about(request):
-    return render(request, "about.html")
+def contact(request):
+    return render(request, "contact.html")
 
 
 def resources(request):
